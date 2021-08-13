e9a83eacfa3d9e604a7c8b332c9627e136c78c9e
Osama Hamad <osamahama2005@gmail.com>
2021-08-13 16:42:58 -0400

    add main menu button functions

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('create-profile-button').addEventListener('click', function hello() {
+    let createProfilePage = document.getElementById('create-profile')
+    createProfilePage.style.display = 'Block'
+})
+
+document.getElementById('add-availability-button').addEventListener('click', function hello() {
+    let createProfilePage = document.getElementById('add-availability')
+    createProfilePage.style.display = 'Block'
+})
