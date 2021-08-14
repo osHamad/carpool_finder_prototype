ae0d3e89e67f668bb72683b25de41104074e85a9
Osama Hamad <osamahama2005@gmail.com>
2021-08-14 10:57:49 -0400

    modified model

--- contactModel.js
+++ contactModel.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 // Setup schema
-var contactSchema = mongoose.Schema({
+var contactSchema = mongoose.Schema({ 
     name: {
         type: String,
         required: true
@@ -18,6 +18,6 @@ var contactSchema = mongoose.Schema({
 });
 // Export Contact model
 var Contact = module.exports = mongoose.model('contact', contactSchema);
-module.exports.get = function (callback, limit) {
+module.exports.get = function (callback, limit) { 
     Contact.find(callback).limit(limit);
 }
